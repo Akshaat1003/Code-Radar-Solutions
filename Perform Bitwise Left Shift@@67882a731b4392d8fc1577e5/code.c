6816ae02a6e04ea3b2bf8372dbcd884dd4843ba3
Akshaat <195753948+Akshaat1003@users.noreply.github.com>
2025-01-31 02:36:17 +0530

    Solution to Perform Bitwise Left Shift

--- Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
+++ Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
@@ -4,7 +4,7 @@ int main() {
     int x,shift,i, num;
     scanf("%d%d", &x,&shift);
     for(i=0, i<shift, ++i){
-        num = x << i;
+        num = num << i;
     }
     printf("%d", x);
     return 0;
