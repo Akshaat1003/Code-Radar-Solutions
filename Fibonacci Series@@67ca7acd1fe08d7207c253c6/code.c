9a9027472928fe9222d7bb2bccd6f23570c308c2
Akshaat A Agarwal <akshaatag1003@gmail.com>
2025-03-28 20:42:14 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int fibonacciSeries(int n){
+    int a=0,b=1;
+    if(n==1) printf("%d", a);
+    else if(n==2) printf("%d %d", a,b);
+    else {
+        printf("%d %d", a,b);
+        for(int i=3; i<=n; i++){
+        int c = a+b;
+        printf("%d", c)
+        a,b = b,c;
+    }
+    }
+}
